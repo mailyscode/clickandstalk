974fabc5827c8cc04d6dbd5360fd14ee09171cfb
Diane Marino <dianemarino0@gmail.com>
2020-11-30 17:49:11 +0100

    twitter hashtags

--- app/services/service_twitter.rb
+++ app/services/service_twitter.rb
@@ -1,30 +1,40 @@
-    def scrap_twitter
-    client = Twitter::REST::Client.new do |config|
-      config.consumer_key        = ENV["TWITTER_API_KEY"]
-      config.consumer_secret     = ENV["TWITTER_API_KEY_SECRET"]
-      config.access_token        = @user.twitter_oauth_data["token"]
-      config.access_token_secret = @user.twitter_oauth_data["secret"]
-    end
-    tweets = client.user_timeline(count: 10, include_rts: false)
-    tweets.each do |tweet|
-      @resource = Resource.new(
-        data_type: "twitter",
-        data: { username: tweet.user.screen_name, content: tweet.text, date: tweet.created_at, place: tweet.place.full_name }
-      )
-      @resource.user = @user
-      @resource.save
-    end
-    hashtags = client.user_timeline(count: 10, include_rts: false)
-    hashtags.each do |hashtag|
-      @resource = Resource.new(
-        data_type: "twitter",
-        data: { hashtags: tweet.text.search_by("#") }
-      )
-      @resource.user = @user
-      @resource.save
-    end
+def intialize
+tweets = client.user_timeline(count: 10, include_rts: false)
+end
+
+
+def scrap_twitter
+  client = Twitter::REST::Client.new do |config|
+    config.consumer_key        = ENV["TWITTER_API_KEY"]
+    config.consumer_secret     = ENV["TWITTER_API_KEY_SECRET"]
+    config.access_token        = @user.twitter_oauth_data["token"]
+    config.access_token_secret = @user.twitter_oauth_data["secret"]
+  end
+  tweets = client.user_timeline(count: 10, include_rts: false)
+  tweets.each do |tweet|
+    @resource = Resource.new(
+      data_type: "twitter",
+      data: { username: tweet.user.screen_name, content: tweet.text, date: tweet.created_at, place: tweet.place.full_name }
+    )
+    @resource.user = @user
+    @resource.save
   end
 
+  tweets = client.user_timeline(count: 10, include_rts: false)
+  hashtags = {}
+  tweets.each do |tweet|
+    tweet.text.split.each do |word|
+      if word.chars.first == "#"
+        if hashtags["#{word}"].nil?
+          hashtags["#{word}"] = 1
+        else
+          hashtags["#{word}"] += 1
+        end
+      end
+    end
+  end
+  hashtags_sorted = hashtags.sort_by{ |word, value|-value }.first(5).to_h
+end
 
   def check_grammar
     gbot = Grammarbot::Client.new(api_key: ENV["GRAMAR_BOT_KEY"], language: 'en-US', base_uri: 'http://api.grammarbot.io')
@@ -33,16 +43,13 @@
     # gbot.base_uri = 'http://pro.grammarbot.io'
     tweets = client.user_timeline(count: 10, include_rts: false)
     tweets.each do |tweet|
-      @resource = Resource.new(
+      @resource = Resource.create(
+        user: @user,
         data_type: "twitter",
-        data: { content: tweet.text.check_tweets }
-      @resource.user = @user
-      @resource.save
+        data: { content: gbot.check(tweet.text) }
       )
     end
-    result = gbot.check(tweet).text
   end
-
 
   def profanity
     require "net/http"
