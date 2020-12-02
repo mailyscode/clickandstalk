24f84013d92674e911b4a44bc2763c5eee59f258
Mailys Bouin <d.syliam@gmail.com>
2020-12-02 18:03:52 +0100

    linkedin page done

--- app/javascript/linkedin/linkedin.js
+++ app/javascript/linkedin/linkedin.js
@@ -1,9 +1,14 @@
 const insertScore = () => {
   const meta = document.querySelector('#score');
+  const information = document.querySelector('#missing-info')
   const newScore = document.querySelector('#score-data')
-  if (meta) {
+  const missingInfo = document.querySelector('#missing-data')
+
+  if (meta || information) {
     const score = meta.getAttribute("content")
     newScore.insertAdjacentHTML('beforeend', `<div class="circle"><p>${score}%</p></div>`)
+    const info = JSON.parse(document.querySelector("#missing-information").dataset.info).join('')
+    missingInfo.insertAdjacentHTML('beforeend', `<ul>${info}</ul>`)
   }
 }
 
