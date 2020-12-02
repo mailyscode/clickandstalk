a9a0e21fb49a26e3ba3cf9e325efb50e4c28b4b8
Diane Marino <dianemarino0@gmail.com>
2020-12-02 16:03:42 +0100

    grammar

--- app/services/service_twitter.rb
+++ app/services/service_twitter.rb
@@ -1,73 +1,98 @@
+require 'net/http'
+
 class ServiceTwitter < ApplicationService
-  def intialize
-  @tweets = client.user_timeline(count: 10, include_rts: false)
-    client = Twitter::REST::Client.new do |config|
+  def initialize(user_id)
+    @user = User.find(user_id)
+    @client = Twitter::REST::Client.new do |config|
       config.consumer_key        = ENV["TWITTER_API_KEY"]
       config.consumer_secret     = ENV["TWITTER_API_KEY_SECRET"]
       config.access_token        = @user.twitter_oauth_data["token"]
       config.access_token_secret = @user.twitter_oauth_data["secret"]
     end
-    @tweets = client.user_timeline(count: 10, include_rts: false)
+    @tweets = @client.user_timeline(count: 10, include_rts: false)
+  end
+
+  def call
+    collect_tweet
+    check_grammar
+    # profanity
+    collect_hashtags
+  end
+
+  private
+
+  def collect_tweet
     @tweets.each do |tweet|
       @resource = Resource.new(
         data_type: "twitter",
-        data: { username: tweet.user.screen_name, content: tweet.text, date: tweet.created_at, place: tweet.place.full_name }
+        data: {
+          username: tweet.user.screen_name,
+          content: tweet.text,
+          date: tweet.created_at,
+          place: tweet.place.full_name,
+          mistake: [],
+          profanity: {}
+        }
       )
       @resource.user = @user
-      @resource.save
+      @resource.save!
     end
-
-     def call
-      check_grammar
-      profanity
-      collect_hashtags
-    end
-
-     private
+  end
 
   def check_grammar
-     gbot = Grammarbot::Client.new(api_key: ENV["GRAMAR_BOT_KEY"], language: 'en-US', base_uri: 'http://api.grammarbot.io')
-      # gbot.api_key = 'new_api_key'
-      # gbot.language = 'en-GB'
-      # gbot.base_uri = 'http://pro.grammarbot.io'
-      @tweets.each do |tweet|
-        @resource = Resource.create(
-          user: @user,
-          data_type: "twitter",
-          data: { content: gbot.check(tweet.text) }
-        )
-      end
-    end
+    gbot = Grammarbot::Client.new(api_key: ENV["GRAMAR_BOT_KEY"], language: 'en-US', base_uri: 'http://api.grammarbot.io')
+    # gbot.api_key = 'new_api_key'
+    # gbot.language = 'en-GB'
+    # gbot.base_uri = 'http://pro.grammarbot.io'
 
-
-    def collect_hashtags
-      hashtags = {}
-      @tweets.each do |tweet|
-        tweet.text.split.each do |word|
-          if word.chars.first == "#"
-            if hashtags["#{word}"].nil?
-              hashtags["#{word}"] = 1
-            else
-              hashtags["#{word}"] += 1
-            end
+    @user.resources.where(data_type: "twitter").each do |resource|
+      checked = gbot.check(resource.data["content"])
+      checked.matches.each do |check|
+        if check.rule['issueType'] == "grammar" || check.rule['issueType'] == "misspelling"
+          data = resource.data
+          data["mistake"] << [check.rule['issueType'], check.context.text[check.offset...check.offset + check.length]]
+          resource.update(data: data)
+          if check.rule['issueType'] == ""
           end
         end
       end
-      @hashtags_sorted = hashtags.sort_by{ |word, value|-value }.first(5).to_h
     end
+  end
+
+  def collect_hashtags
+    @tweets.each do |tweet|
+      tweet.text.split.each do |word|
+        if word.chars.first == "#"
+          if hashtags["#{word}"].nil?
+            hashtags["#{word}"] = 1
+          else
+            hashtags["#{word}"] += 1
+          end
+        end
+      end
+    end
+    Resource.create!(
+      data_type: 'twitter',
+      data: {
+        hashtag: hashtags.sort_by { |word, value| -value }.first(5)
+      },
+      user: @user
+    )
+    resource.update(data: data)
+  end
 
   def profanity
-    require "net/http"
-      url = URI("https://api.promptapi.com/bad_words?censor_character=*")
+    url = URI("https://api.promptapi.com/bad_words?censor_character=*")
 
-      https = Net::HTTP.new(url.host, url.port);
-      https.use_ssl = true
+    https = Net::HTTP.new(url.host, url.port);
+    https.use_ssl = true
 
-      request = Net::HTTP::Post.new(url)
-      request['apikey'] = "P9EuaEEertmGmaMxaaWhmpQzLTKtM36i"
-      request.body = ""
-      response = https.request(request)
-      puts response.read_body
+    responses = []
+    request = Net::HTTP::Post.new(url)
+    request['apikey'] = "P9EuaEEertmGmaMxaaWhmpQzLTKtM36i"
+    @tweets.each do |tweet|
+      request.body = tweet.text
+      responses << https.request(request).read_body
     end
   end
 end
