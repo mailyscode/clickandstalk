bf417d249dd1b7c8ec55ae292493cef551ce09a7
Diane Marino <dianemarino0@gmail.com>
2020-12-03 12:46:24 +0100

    aaa

--- app/jobs/scrapping_job.rb
+++ app/jobs/scrapping_job.rb
@@ -2,8 +2,9 @@ class ScrappingJob < ApplicationJob
   queue_as :default
 
   def perform(user_id)
-    ScrappingInsta.new(user_id).perform
-    ScrappingLinkedin.new(user_id).perform
+    @user = User.find(user_id)
+    # ScrappingInsta.new(user_id).perform unless @user.username_insta
+    ScrappingLinkedin.new(user_id).perform if @user.username_linkedin
     # ScrappingTwitter.new(user_id).perform
     # send email
     # mail = UserMailer.job_finish
